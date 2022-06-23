411bc668a4119e78fcae4e5ec29c58b47ef9660e
Jules <jules.brutschy@gmail.com>
2022-06-23 10:42:53 +0200

    Version BDD site 1

--- groupe1/jeb/urls.py
+++ groupe1/jeb/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path("capteur/info.html",views.capteur),
-    path("update/<int:id>/", views.update),
-    path("updatetraitement/<int:id>/", views.updatetraitement),
-    path("donnee/info.html", views.donnee)
+    path('capteur/update/<int:id>', views.update),
+    path('capteur/info/<int:id>', views.updatetraitement),
+    path("donnee/info.html", views.donnee),
+    path('capteur/info/<int:id>', views.export_csv),
 ]
--- groupe1/jeb/views.py
+++ groupe1/jeb/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render
 from .models import sensors1, sensors_data
 from .forms import SensorsForm
+from django.http import HttpResponseRedirect , HttpResponse
+from django.forms.models import model_to_dict
+import csv
 
 
 def index(request):
@@ -16,19 +19,49 @@ def donnee(request):
 
 def update(request, id):
     sensors = sensors1.objects.get(pk=id)
-    form = SensorsForm(sensors1.dico())
-    return render(request, "capteur/info.html",{"form":form, "id":id})
+    form = SensorsForm(model_to_dict(sensors))
+    if request.method == "POST":
+        form = SensorsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/capteur/info")
+    else:
+        return render(request, "capteur/update.html", {"form": form, "id": id})
+
 
 def updatetraitement(request, id):
-    form = SensorsForm(request.POST)
-    if form.is_valid():
-        sensors = form.save(commit=False)
+    sensors = SensorsForm(request.POST)
+    bak = sensors1.objects.get(id=id)
+    if sensors.is_valid():
+        sensors = sensors.save(commit=False)
         sensors.id = id
+        sensors.macaddr = bak.macaddr
+        sensors.piece = bak.piece
         sensors.save()
-        return HttpResponseRedirect("/info.html")
+        return HttpResponseRedirect("/capteur/info.html")
     else:
-        return render(request, "capteur/update.html", {"form": form, "id":id})
+        return render(request, "capteur/info.html", {"form": sensors, "id": id})
 
+def export_csv(request, id):
+    data = sensors_data.objects.filter(sensor = id)
+
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="export.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(['timestamp', 'macaddr', 'piece', 'emplacement', 'nom', 'temp'])
+    for i in data:
+        writer.writerow(
+            [i.datetime,
+            i.sensor.macaddr,
+            i.sensor.piece,
+            i.sensor.emplacement,
+            i.sensor.nom,
+            i.temp])
+
+    return response
 
 """
 def delete(request, id):
